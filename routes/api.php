1894e82bdf7743888747ee3dfc55b9ccec17fd30
Selene_Rojas <desarrollo.13@nexen-elog.com>
2025-09-26 13:42:18 -0600

    agrgado use fake

--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,5 @@ Route::get('/ping', function () {
     return response()->json(['pong' => true]);
 });
 Route::apiResource('users', UserController::class);
+Route::get('users', UserController::class);
 Route::get('pokemon/{name}', [PokemonController::class, 'show']);
