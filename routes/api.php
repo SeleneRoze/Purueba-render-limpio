47620727cbda31c9cefae25c4c64274d595bc7d0
Selene_Rojas <desarrollo.13@nexen-elog.com>
2025-09-30 17:28:12 -0600

    cambio a psql

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,17 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\PokemonController;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/ping', function () {
     return response()->json(['pong' => true]);
 });
 Route::apiResource('users', UserController::class);
 Route::get('pokemon/{name}', [PokemonController::class, 'show']);
+// routes/web.php
+
+
+Route::get('/run-seeders', function() {
+    Artisan::call('db:seed', ['--force' => true]);
+    return "Seeders ejecutados correctamente 😎";
+});
